2a7abad147177032588cc9d2f649da03b85d317d
Hantsa <hantsaramefison@gmail.com>
2025-12-09 14:29:23 +0100

    ajout explications dans client et serveur

--- serveur.io.js
+++ serveur.io.js
@@ -20,6 +20,9 @@ var joueurs = [];
 var jeton = -1;
   
 
+/*===============================
+Ecouteurs d'événements socket pris du TP2 + rajout de notre part
+================================*/
 
 io.on('connection', (socket) => {
 
@@ -34,14 +37,18 @@ io.on('connection', (socket) => {
    socket.on('entree', nomJoueur => {
         console.log("Entrée dans la partie de " + nomJoueur);
 
-        if (joueurs.length < nbJoueurs) {
-            if (!joueurs.includes(nomJoueur)) {
+        // si le nb de joueurs maximal n'est pas encore atteint
+        if (joueurs.length < nbJoueurs) 
+        {
+            // si le joueur n'est pas déjà enregistré càd le meme nom, on le met dans le tableau de joueurs
+            if (!joueurs.includes(nomJoueur)) 
+            {
                 joueurs.push(nomJoueur);
                 console.dir(joueurs);
 
                 let nomsJoueurs = "";
                 for (let nom of joueurs) nomsJoueurs += nom + " ";
-
+                
                 socket.emit('entree', {
                     nomJoueur: nomJoueur,
                     numJoueur: joueurs.length - 1,
@@ -52,39 +59,43 @@ io.on('connection', (socket) => {
                     nomsJoueurs: nomsJoueurs
                 });
 
+                // si le nb minimal de joueurs pouvant jouer est atteint, la partie peut commencer
+                // chaque joueur ayant un jeton, ils jouent donc selon un ordre precis : 
+                // le premier joueur ayant rejoint a un jeton 0 donc c'est lui qui joue en premier
+
                 if (joueurs.length === nbJoueurs) {
                     jeton = 0;
                     console.log("Le jeton passe à 0, la partie peut commencer");
                     io.emit('messageServeur', 'la partie peut commencer');
                     io.emit('tour', { joueurCourant: jeton });
                 }
-            } else {
-                socket.emit('messageServeur', 'Nom de joueur déjà enregistré');
-            }
-        } else {
-            socket.emit('messageServeur', 'Nombre de joueurs déjà atteint !');
+            } 
+            else
+                socket.emit('messageServeur', 'Nom de joueur déjà enregistré'); // joueur deja present
         }
+        else
+            socket.emit('messageServeur', 'Nombre de joueurs déjà atteint !'); 
+            // joueurs max atteint càd joueurs.length == nbJoueurs 
     });
 
+    // ecouteur permettant d'ecouter chaque coup des joueurs, donc permet d'établir le tour de rôle
+    socket.on('coupJoue', tour => {
+        console.log("coupJoue reçu de numJoueur =", tour.numJoueur);
 
-    socket.on('coupJoue', data => {
-        console.log("coupJoue reçu de numJoueur =", data.numJoueur);
-
-        if (data.numJoueur === jeton) {
+        if (tour.numJoueur === jeton)
+        {
             jeton = (jeton + 1) % joueurs.length;
             io.emit('tour', { joueurCourant: jeton });
-        } else {
-            socket.emit('messageServeur', "Pas ton tour");
-        }
+        } 
+        else socket.emit('messageServeur', "Pas ton tour");
     });
-
-
 
 
     socket.on('sortie', nomJoueur => {
         console.log("Sortie de la partie de "+nomJoueur);
         let index = joueurs.indexOf(nomJoueur)
-        if  (index != -1) {
+        if (index != -1)
+        {
             joueurs.splice(index, 1);
             jeton = -1;
             let nomsJoueurs = "";
@@ -100,12 +111,12 @@ io.on('connection', (socket) => {
     });
 
     socket.on('message', data => {
-        console.log("Message à diffuser de",data.numJoueur,":",data.texte);
+        console.log("Message à diffuser de [", data.numJoueur, "]:" ,data.texte);
         if (data.numJoueur == -1) 
             socket.emit('messageServeur', 'Vous devez entrer dans la partie !');
         else {
             let message = joueurs[data.numJoueur] + " : " + data.texte;
-            console.log("Message à diffuser :", message)
+            console.log("Message à diffuser =>", message)
             io.emit('message', message);
         }
     });
